6a1799e6ce8e4680cb09d031eda1acee1fd28fb8
Roman <vikulov.roman.work@gmail.com>
2026-03-06 09:58:43 +0200

    Initial weather app

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/weather_app/",
+  base: '/weather_app/',
   plugins: [react()],
 })
