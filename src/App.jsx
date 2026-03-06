cc07ad38c9b871c744446add97ab633f58831c2a
Roman <vikulov.roman.work@gmail.com>
2026-03-06 09:44:37 +0200

    added an offline marker

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,20 @@ import '../App.css';
 
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 /* ===== WEATHER TRANSLATE ===== */
 
 const translateWeather = (desc) => {
